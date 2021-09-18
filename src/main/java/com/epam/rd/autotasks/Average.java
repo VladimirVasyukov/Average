147e939b0e8755f4e28347c885afb98881b89dc7
Vladimir Vasyukov <vladimir.vasyukov7@gmail.com>
2021-09-18 14:59:58 +0300

    Java Fundamentals - Average

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -1,12 +1,29 @@
 package com.epam.rd.autotasks;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+/**
+ * 04_Java_Fundamentals - Vladimir Vasyukov
+ * Application's entry point, use it to demonstrate code execution
+ */
 public class Average {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
+        Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8.name());
+        int number;
+        int sumOfNumbers = 0;
+        int amountOfNumbers = 0;
+        int average;
+        do {
+            number = scanner.nextInt();
+            sumOfNumbers += number;
+            amountOfNumbers++;
+        } while (number != 0);
 
+        if (number == 0) {
+            average = sumOfNumbers / (amountOfNumbers - 1);
+            System.out.println(average);
+        }
     }
-
 }
